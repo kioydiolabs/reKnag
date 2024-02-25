36a0a35e42b1f1c5460da100d1a931410819d34a
Stratos Thivaios <stratosthivaios@outlook.com.gr>
2024-02-25 11:50:38 +0200

    Created the whole lookup website

---
+++ reknag-lookup/index.js
@@ -0,0 +1,77 @@
+document.getElementById("submit").onclick = function(){
+    original_url = document.getElementById("url").value;
+    endpoint = "https://shortapi.kioydio.org/check/?id="+original_url
+    console.log(endpoint)
+
+    const Http = new XMLHttpRequest();
+    Http.open("GET", endpoint);
+    Http.send();
+
+    Http.onreadystatechange=(e)=>{
+        document.getElementById("output").innerHTML = Http.responseText;
+        document.getElementById("link").href = Http.responseText;
+        console.log(Http.response);
+        var output = Http.responseText;
+    }
+
+    document.getElementById("viewfull").style.display = "block";
+    document.getElementById("vtotal").style.display = "block";
+
+}
+
+document.getElementById("vtotal").onclick = function(){
+    output = document.getElementById("output").innerHTML;
+    endpoint = "https://shortapi.kioydio.org/get_vt_total_link/?url="+output
+    console.log("heh",output)
+
+    const Http = new XMLHttpRequest();
+    Http.open("GET", endpoint);
+    Http.send();
+
+    Http.onloadend=(e)=>{
+        var vtotalink = Http.responseText;
+        window.open(vtotalink,"_blank")
+    }
+
+}
+
+document.getElementById("viewfull").onclick = function(){
+    original_url = document.getElementById("url").value;
+    endpoint = "https://shortapi.kioydio.org/check/?id="+original_url
+    console.log(endpoint)
+
+    const Http = new XMLHttpRequest();
+    Http.open("GET", endpoint);
+    Http.send();
+
+    Http.onloadend=(e)=>{
+        window.alert(Http.responseText);
+    }
+}
+
+
+document.getElementById("copy").onclick = function(){
+    console.log("copy clicked")
+    var copyText = document.getElementById("output");
+
+    /* Create a temporary textarea element to copy the text */
+    var tempTextarea = document.createElement("textarea");
+    tempTextarea.value = copyText.textContent;
+
+    /* Append the textarea to the document */
+    document.body.appendChild(tempTextarea);
+
+    /* Select and copy the text */
+    tempTextarea.select();
+    document.execCommand("copy");
+
+    /* Remove the temporary textarea */
+    document.body.removeChild(tempTextarea);
+
+    /* Provide some visual feedback (optional) */
+    document.getElementById("confirm").style.opacity = "1";
+    setTimeout(function(){
+        document.getElementById("confirm").style.opacity = "0";
+    },2000);
+}
+
--- server_side/api.py
+++ server_side/api.py
@@ -2,6 +2,7 @@ import random
 import string
 import os
 from flask import Flask, request, jsonify, Response
+import json
 
 app = Flask(__name__)
 
@@ -26,9 +27,9 @@ def create():
 
     js_code = f"window.location.replace(\"{original_url}\")"
 
-    with open(f'/var/www/html/index.html', 'w', encoding='utf-8') as file:
+    with open(f'/var/www/html/{random_string}/index.html', 'w', encoding='utf-8') as file:
         file.write(html_code)
-    with open(f'/var/www/html/index.js', 'w', encoding='utf-8') as file:
+    with open(f'/var/www/html/{random_string}/index.js', 'w', encoding='utf-8') as file:
         file.write(js_code)
 
     # print(f"\n\n\nYour shortened URL is : http://127.0.0.1:3000/{random_string}")
@@ -38,6 +39,51 @@ def create():
 
     return Response(url_final, mimetype='text/txt')
 
+@app.route("/check/")
+def check():
+
+    id = request.args.get("id")
+
+    with open(f"/var/www/html/{id}/index.js", "r") as file:
+        jscont = file.read()
+
+    #jscont = str(open(f"/var/www/html/{id}/index.js"))
+
+    char = len(jscont)-2
+    url = jscont[25:char]
+
+    # print(f"\n\n\nYour shortened URL is : http://127.0.0.1:3000/{random_string}")
+    # uncomment the line above for debugging purposes
+
+    return Response(url, mimetype='text/txt')
+
+
+@app.route("/get_vt_total_link/")
+def getvttotallink():
+    urltoget = request.args.get("url")
+    
+    url = "https://www.virustotal.com/api/v3/urls"
+
+    payload = { "url": f"{urltoget}" }
+    headers = {
+        "accept": "application/json",
+        "x-apikey": "your_virustotal_api_key",
+        "content-type": "application/x-www-form-urlencoded"
+    }
+
+    response = requests.post(url, data=payload, headers=headers)
+    parsed = json.loads(response.text)
+    filtered_data = parsed['data']['links']['self']
+    parts = filtered_data.split("-")
+    id = parts[-2]
+    vturl=f"https://www.virustotal.com/gui/url/{id}"
+
+    resp = Response(vturl)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
