afe3a6278efa3e7dadb96b3c940e95dc27fe9702
Stratos Thivaios <stratosthivaios@outlook.com.gr>
2024-03-23 19:17:08 +0200

    working version of update_api.sh

--- scripts/update_api.sh
+++ scripts/update_api.sh
@@ -1,10 +1,10 @@
 cd /etc/
 mkdir reknag
 cd reknag
-git clone github.com/kioydiolabs/reKnag temp_update
+git clone https://github.com/kioydiolabs/reKnag temp_update
 cd temp_update
 cd server_side
 cp api.py /etc/api_short/api.py
-systemctl restart reknag_api
+# systemctl restart reknag_api
 cd ../../
 rm -rf temp_update
